5e3e53a889af5eb243b8f64edd6c30c2a8bda259
Dmitrii <lomonovskij@gmail.com>
2020-08-28 17:09:45 +0300

    commit

--- app-assets/js/scripts/ui/data-list-view.js
+++ app-assets/js/scripts/ui/data-list-view.js
@@ -17,37 +17,42 @@ $(document).ready(function() {
         orderable: true,
         targets: 0,
         // checkboxes: { selectRow: true }
-      }
+      },
     ],
     dom:
-      '<"top"<"actions action-btns"B><"action-filters"lf>><"clear">rt<"bottom"<"actions">p>',
+      '<"top"<"actions action-btns"B> <"action-filters"fll>>     <"clear">rt<"bottom" <"actions"> p>',
+    
+ 
     oLanguage: {
       sLengthMenu: "_MENU_",
-      sSearch: ""
+      sSearch: "",
     },
-    aLengthMenu: [[4, 10, 15, 20], [4, 10, 15, 20]],
+    aLengthMenu: [
+      [4, 10, 15, 20],
+      [4, 10, 15, 20],
+    ],
     select: {
-      style: "multi"
+      style: "multi",
     },
     order: [[1, "asc"]],
     bInfo: false,
     pageLength: 4,
     buttons: [
       {
-        text: "<i class='feather icon-plus'></i> Add New",
-        action: function() {
-          $(this).removeClass("btn-secondary")
-          $(".add-new-data").addClass("show")
-          $(".overlay-bg").addClass("show")
-          $("#data-name, #data-price").val("")
-          $("#data-category, #data-status").prop("selectedIndex", 0)
+        text: "<i class='feather icon-plus'></i> Добавить +",
+        action: function () {
+          $(this).removeClass("btn-secondary");
+          $(".add-new-data").addClass("show");
+          $(".overlay-bg").addClass("show");
+          $("#data-name, #data-price").val("");
+          $("#data-category, #data-status").prop("selectedIndex", 0);
         },
-        className: "btn-outline-primary"
-      }
+        className: "btn-outline-primary",
+      },
     ],
-    initComplete: function(settings, json) {
-      $(".dt-buttons .btn").removeClass("btn-secondary")
-    }
+    initComplete: function (settings, json) {
+      $(".dt-buttons .btn").removeClass("btn-secondary");
+    },
   });
 
   dataListView.on('draw.dt', function(){
